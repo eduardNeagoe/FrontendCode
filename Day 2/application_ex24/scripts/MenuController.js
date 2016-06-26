d2b7fd50da6061d11efe8fbc253db71fba0b27ac
George Furnica <eduard.c.neagoe@gmail.com>
2016-06-26 22:19:12 +0300

    Exercise 24 (navbar menu)

---
+++ Day 2/application_ex24/scripts/MenuController.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Eduard on 24.06.2016.
+ */
+hrApp.controller('MenuController', ['$rootScope', '$scope', function($rootScope, $scope){
+    $scope.employeeActionList = [
+        {
+            label:'List Employees',
+            url: ''
+        }
+    ];
+}]);
